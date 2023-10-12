c4c87bdc73a9ceb716090477f87917641f687fc0
zorziristu <146484264+zorziristu@users.noreply.github.com>
2023-10-12 22:25:30 +0300

    Добавила файлы домашнего задания

---
+++ lesson1/dz.py
@@ -0,0 +1,15 @@
+a = []
+h = 'чо писать?'
+b = input(' ')
+a.append(b)
+for i in range(len(a)):
+ 
+   if b == h:
+        #a[i].upper() эта не работает
+        #b.replace(b, 'ЧО ПИСАТЬ?') 
+        print('ЧО ПИСАТЬ?')
+        break
+    
+
+# остольное я не знаю как сделать 
+
